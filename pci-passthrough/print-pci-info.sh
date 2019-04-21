fa6549e7519395874974ee57c4f6d8b0c7172308
Fernando Escribano <f.escrb@gmail.com>
2019-04-22 00:41:52 +0100

    Added iommu group info to script

--- pci-passthrough/print-pci-info.sh
+++ pci-passthrough/print-pci-info.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-# Set the delimiter for the 'in' keyword be a newline rather than any whitespace
-IFS=$'\n'
-for DEVICE in $(lspci -nn);
+
+NUM_DEVICE_GROUPS="$(ls /sys/kernel/iommu_groups/ | wc -w)"
+for DEVICE_GROUP in $(seq 0 $NUM_DEVICE_GROUPS);
 do
-    BUS_ID="$(echo $DEVICE | cut -f 1 -d " ")"
-    MODALIAS="$(cat /sys/bus/pci/devices/0000:$BUS_ID/modalias)"
-    echo $DEVICE $MODALIAS
+    DEVICES_PATH="/sys/kernel/iommu_groups/$DEVICE_GROUP/devices/"
+    if [ -d "$DEVICES_PATH" ]; then
+        for DEVICE in $(ls $DEVICES_PATH);
+        do
+            MODALIAS="$(cat /sys/bus/pci/devices/$DEVICE/modalias)"
+            BUS_ID="$(echo $DEVICE | cut -f 2,3 -d ":")"
+            LSPCI_DEVICE_LINE=$(lspci -nn | grep $BUS_ID)
+            echo "Group $DEVICE_GROUP: $LSPCI_DEVICE_LINE $MODALIAS" 
+        done
+    fi
 done
